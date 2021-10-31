bec5ed3df95510f0e66417e49c0e082cf6711f4e
Fiddlekins <fiddlekins@gmail.com>
2021-10-31 23:06:25 +0000

    Swapped axis from market cap to total supply

--- src/Graph.js
+++ src/Graph.js
@@ -3,8 +3,8 @@ import styles from './Graph.module.css';
 
 export default function Graph({amplData}) {
 	const x = amplData.map(({price}) => price);
-	const y = amplData.map(({market_cap}) => market_cap);
-	const z = amplData.map(({total_volume}) => total_volume);
+	const y = amplData.map(({totalSupply}) => totalSupply);
+	const z = amplData.map(({totalVolume}) => totalVolume);
 	const days = amplData.map(({daysSinceStart}) => daysSinceStart);
 	const data = [
 		{
@@ -25,6 +25,11 @@ export default function Graph({amplData}) {
 		autosize: true,
 		title: 'AMPL historical data',
 		scene: {
+			aspectratio: {
+				x: 1,
+				y: 1,
+				z: 1
+			},
 			xaxis: {
 				title: {
 					text: 'Price'
@@ -32,7 +37,7 @@ export default function Graph({amplData}) {
 			},
 			yaxis: {
 				title: {
-					text: 'Market Cap'
+					text: 'Total Supply'
 				},
 				type: 'log',
 				autorange: true
--- src/getAmplData.js
+++ src/getAmplData.js
@@ -1,8 +1,64 @@
+import {ApolloClient, gql, InMemoryCache} from '@apollo/client';
+
 const dayMs = 1000 * 60 * 60 * 24;
+export const client = new ApolloClient({
+	uri: 'https://api.thegraph.com/subgraphs/name/aalavandhan/ampleforth',
+	cache: new InMemoryCache(),
+});
+
+export const GET_REBASES = gql`
+  query getRebases($first: Int, $lastEpoch: BigInt!) {
+    rebases(first: $first, orderBy: epoch, where: { epoch_gte: $lastEpoch }) {
+      id,
+      supply,
+      epoch
+    }
+  }
+`;
+
+async function getTotalSupplyData() {
+	const data = [];
+	const ids = new Set();
+	const first = 1000;
+	let iterate = true;
+	let lastEpoch = 0;
+	while (iterate) {
+		const rebasesResponse = await client.query({
+			query: GET_REBASES,
+			variables: {
+				first,
+				lastEpoch
+			}
+		});
+		const {rebases} = rebasesResponse.data;
+		iterate = rebases.length >= first;
+		lastEpoch = parseInt(rebases[rebases.length - 1].epoch, 10);
+		for (const rebase of rebases) {
+			if (!ids.has(rebase.id)) {
+				data.push(rebase);
+				ids.add(rebase.id);
+			}
+		}
+	}
+	return data.map(({supply, epoch}) => {
+		return {
+			epoch: parseInt(epoch, 10),
+			totalSupply: parseFloat(supply)
+		};
+	});
+}
+
+async function getMarketData() {
+	const res = await fetch(`https://api.coingecko.com/api/v3/coins/ampleforth/market_chart?vs_currency=usd&days=max`);
+	return res.json();
+}
 
 export default async function getAmplData() {
-	const res = await fetch(`https://api.coingecko.com/api/v3/coins/ampleforth/market_chart?vs_currency=usd&days=max`);
-	const {market_caps, prices, total_volumes} = await res.json();
+	const [totalSupplyData, marketData] = await Promise.all([
+		getTotalSupplyData(),
+		getMarketData()
+	]);
+	const {market_caps, prices, total_volumes} = marketData;
 	const aggregatedData = {};
 	for (const [timestamp, value] of market_caps) {
 		if (!aggregatedData[timestamp]) {
@@ -24,13 +80,16 @@ export default async function getAmplData() {
 	}
 	const sortedTimestamps = Object.keys(aggregatedData).map(key => parseInt(key, 10)).sort((a, b) => a - b);
 	const timestampStart = sortedTimestamps[0];
-	return sortedTimestamps.map(timestamp => {
+	return sortedTimestamps.map((timestamp, index) => {
 		const {market_cap, price, total_volume} = aggregatedData[timestamp];
+		const {totalSupply} = totalSupplyData[index];
+		const daysSinceStart = (timestamp - timestampStart) / dayMs;
 		return {
-			market_cap,
+			marketCap: market_cap,
 			price,
-			total_volume,
-			daysSinceStart: (timestamp - timestampStart) / dayMs
+			totalVolume: total_volume,
+			totalSupply,
+			daysSinceStart
 		};
 	});
 }
